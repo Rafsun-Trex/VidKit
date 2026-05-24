51a2ecc1ae6404f1012ab47d63fcd7e5675e1ad3
Rafsun Eshrar <rafsun.twinbit@gmail.com>
2026-05-24 09:49:06 +0600

    Restore player preview background

--- VidKit/Editor/VideoPlayerView.swift
+++ VidKit/Editor/VideoPlayerView.swift
@@ -35,7 +35,7 @@ final class VideoPlayerView: UIView {
 
     private func setup() {
         playerLayer.videoGravity = .resizeAspect
-        backgroundColor = .clear
+        backgroundColor = .black
         layer.cornerRadius = 12
         clipsToBounds = true
     }
